299b0c05f130e9240ff8b2e992091665db96d54b
Your Name <your-email@example.com>
2025-12-12 11:02:53 +0530

    Praticed MergeSort

--- DividenConquer.java
+++ DividenConquer.java
@@ -123,7 +123,7 @@ public class DividenConquer {
         }
     }
 
-public static int search1(int arr[], int tar, int si, int ei){
+    public static int search1(int arr[], int tar, int si, int ei){
     while(si <= ei){
         int mid = si + (ei - si)/2;
 
@@ -154,6 +154,47 @@ public static int search1(int arr[], int tar, int si, int ei){
     return -1;
 }
 
+    public static void mergeSort1(int arr[], int si, int ei){
+        if(si >= ei){
+            return;
+        }
+
+        int mid = si + (ei - si)/2;
+        mergeSort1(arr, si, mid);
+        mergeSort1(arr, mid+1, ei);
+
+        merge1(arr, si, mid, ei);
+    }
+    public static void merge1(int arr[], int si, int mid, int ei){
+        int temp[] = new int[ei - si +1];
+        int i = si;
+        int j = mid+1;
+        int k = 0;
+
+        while(i <= mid && j <= ei){
+            if(arr[i] < arr[j]){
+                temp[k] = arr[i];
+                i++;
+            }
+            else{
+                temp[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i <= mid){
+            temp[k++] = arr[i++];
+        }
+        while(j <= ei){
+            temp[k++] = arr[j++];
+        }
+
+        for(k = 0, i = si; k < temp.length; i++, k++){
+            arr[i] = temp[k];
+        }
+    }
+
     public static void main(String[] args) {
         // int arr[] = {6,3,-8,9,0,5,8,2,-2};
         // mergeSort(arr, 0, arr.length-1);
@@ -163,9 +204,25 @@ public static int search1(int arr[], int tar, int si, int ei){
         // quickSort(arr, 0, arr.length-1);
         // printArr(arr);
 
-        int arr[] = {6,7,8,9,22,2,4,5};
-        int target = 9;
-        System.out.println("Search Method: "+search(arr, target, 0, arr.length-1));
-        System.out.println("Search Method 1: "+search1(arr, target, 0, arr.length-1));
+        // int arr[] = {6,7,8,9,22,2,4,5};
+        // int target = 9;
+        // System.out.println("Search Method: "+search(arr, target, 0, arr.length-1));
+        // System.out.println("Search Method 1: "+search1(arr, target, 0, arr.length-1));
+
+        int arr[] = {4,5,9,1,11};
+        int si = 0;
+        int ei = arr.length-1;
+        mergeSort(arr, si, ei);
+        System.out.print("Merged using Method 1: ");
+        printArr(arr);
+        System.out.println();
+        
+        int arr1[] = {4,5,9,1,11};
+        int si1 = 0;
+        int ei1= arr1.length-1;
+        mergeSort1(arr1, si1, ei1);
+        System.out.print("Merged using Method 2: ");
+        printArr(arr1);
+        System.out.println();
     }
 }
