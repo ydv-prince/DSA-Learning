2c92685e0914978412b1de4c089eb3bcb4d56c15
Your Name <your-email@example.com>
2025-12-13 11:16:14 +0530

    Quick Sort Practice

--- practice.java
+++ practice.java
@@ -71,4 +71,45 @@ public class practice {
             arr[i] = temp[k];
         }
     }
+
+    public static void quickSort(int arr[], int si, int ei){
+        //Base case
+        if(si >= ei){
+            return;
+        }
+
+        int pIdx = partition(arr, si, ei);
+        quickSort(arr, si, pIdx-1);
+        quickSort(arr, pIdx+1, ei);
+    }
+
+    public static int partition(int arr[], int si, int ei){
+        int pivot = arr[ei];
+        int i = si-1;
+
+        for(int j=si; j < ei; j++){
+            if(arr[j] <= pivot){
+                i++;
+                int temp = arr[j];
+                arr[ei] = arr[i];
+                arr[i] = temp;
+            }
+        }
+
+        i++;
+        int temp = pivot;
+        arr[ei] = arr[i];
+        arr[i] = temp;
+        return i;
+    }
+
+    //Main method
+    public static void main(String[] args) {
+        int arr[] = {5,6,7,13,3,9,};
+        int si = 0;
+        int ei = arr.length-1;
+        quickSort(arr, si, ei);
+        System.out.print("Quick Sort: ");
+        DividenConquer.printArr(arr);
+    }
 }
