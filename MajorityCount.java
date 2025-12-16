044fd7ced5c2d77afa29587d5de8c3de177d0ad4
Your Name <your-email@example.com>
2025-12-16 23:56:32 +0530

    Question on Majority count

---
+++ MajorityCount.java
@@ -0,0 +1,44 @@
+public class MajorityCount {
+    /*
+    Q2. Given an array nums of size n, return the majority element the majority element is the element that appears more then (n/2). You may assume that the majority element always exists in the array.
+     */
+
+    public static int countInRange(int[] nums, int num, int lo, int hi){
+        int count = 0;
+        for(int i = lo; i <= hi; i++){
+            if(nums[i] == num){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static int majorityElementRec(int[] nums, int lo, int hi){
+        if(lo == hi){
+            return nums[lo];
+        }
+
+        int mid = (hi-lo)/2 + lo;
+        int left = majorityElementRec(nums, lo, mid);
+        int right = majorityElementRec(nums, mid+1, hi);
+
+        if(left == right){
+            return left;
+        }
+        
+        int leftCount = countInRange(nums, left, lo, hi);
+        int rightCount = countInRange(nums, right, lo, hi);
+
+        return leftCount > rightCount ? left : right;
+    }
+
+    public static int majorityElement(int[] nums){
+        return majorityElementRec(nums, 0, nums.length-1);
+    }
+
+    public static void main(String[] args) {
+        int nums[] = {2,2,1,2,1,1,1,4,5,1};
+        System.out.print("Majority Count: ");
+        System.out.println(majorityElement(nums));
+    }
+}
