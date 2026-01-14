21c033ab4463d12f1cdbcb47af0ad71f5a1aee5d
ydv-prince <psychoaimerhere@gmail.com>
2026-01-14 11:12:26 +0530

    First program

--- First.java
+++ First.java
@@ -4,9 +4,21 @@ class First {
     public First() {
         name = "Prince";
     }
+    public static void print(int[] arr){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
 
     public static void main(String[] args) {
-        First first = new First();
-        System.out.println("Hello, " + first.name);
+        // First first = new First();
+        // System.out.println("Hello, " + first.name);
+
+        int letterFrequency[] = new int[26];
+        print(letterFrequency);
+        int currentIndex = 's' - 'a';
+        letterFrequency[currentIndex]++;
+        print(letterFrequency);
     }
 }
