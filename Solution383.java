83165b7378a12b9298f66cae52c79656b343500d
ydv-prince <psychoaimerhere@gmail.com>
2026-01-14 11:11:25 +0530

    383. Ransom Note

--- Solution383.java
+++ Solution383.java
@@ -1,29 +1,51 @@
+//383. Ransom Note
 public class Solution383 {
-    public static  boolean canConstruct(String ransomNote, String magazine) {
+    // public static  boolean canConstruct(String ransomNote, String magazine) {
+    //     if(ransomNote.length() > 0 && magazine.length() == 0) return false;
+
+    //     int[] letterFrequency = new int[26];
+    //     for(int i=0; i<magazine.length(); i++){
+    //         char currentChar = magazine.charAt(i);
+    //         int index = currentChar - 'a';
+    //         letterFrequency[index]++;
+    //     }
+
+    //     for(int i=0; i<ransomNote.length(); i++){
+    //         char currentChar = ransomNote.charAt(i);
+    //         int index = currentChar - 'a';
+    //         letterFrequency[index]--;
+
+    //         if(letterFrequency[index] < 0){
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+
+
+    public static boolean canConstruct(String ransomNote, String magazine) {
         if(ransomNote.length() > 0 && magazine.length() == 0) return false;
 
         int[] letterFrequency = new int[26];
-        for(int i=0; i<magazine.length(); i++){
-            char currentChar = magazine.charAt(i);
-            int index = currentChar - 'a';
-            letterFrequency[index]++;
+        for(char chr : magazine.toCharArray()){
+            letterFrequency[chr - 'a']++;
         }
 
-        for(int i=0; i<ransomNote.length(); i++){
-            char currentChar = ransomNote.charAt(i);
-            int index = currentChar - 'a';
-            letterFrequency[index]--;
+        for(char chr : ransomNote.toCharArray()){
+            letterFrequency[chr - 'a']--;
 
-            if(letterFrequency[index] < 0){
+            if(letterFrequency[chr - 'a'] < 0){
                 return false;
             }
         }
         return true;
     }
+
+
     public static void main(String[] args) {
-        // String ransomNote = "a", magazine = "b";
+        String ransomNote = "a", magazine = "b";
         // String ransomNote = "aa", magazine = "ab";
-        String ransomNote = "aa", magazine = "aab";
+        // String ransomNote = "aa", magazine = "aab";
         System.out.println(canConstruct(ransomNote, magazine));
     }
 }
