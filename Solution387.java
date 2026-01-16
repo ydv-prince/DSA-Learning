25e2716a40169c1208eca7bc2398849fb4eca582
ydv-prince <psychoaimerhere@gmail.com>
2026-01-16 21:53:27 +0530

    387. First Unique Character in a String

---
+++ Solution387.java
@@ -0,0 +1,10 @@
+//387. First Unique Character in a String
+public class Solution387 {
+    public int firstUniqChar(String s) {
+        for(int i=0; i<String.length(); i++){
+            if(String.charAt(i)){
+                
+            }
+        }
+    }
+}
