a2c1195054a1129bdf89fbeeeee0079d8da2ec01
ydv-prince <psychoaimerhere@gmail.com>
2026-01-16 22:07:36 +0530

    387. First Unique Character in a String

--- Solution387.java
+++ Solution387.java
@@ -1,10 +1,22 @@
 //387. First Unique Character in a String
 public class Solution387 {
-    public int firstUniqChar(String s) {
-        for(int i=0; i<String.length(); i++){
-            if(String.charAt(i)){
-                
+    public static int firstUniqChar(String s) {
+        int frequency[] = new int[26];
+        int n = s.length();
+        for(int i=0; i<n; i++){
+            frequency[s.charAt(i) - 'a']++;
+        }
+        for(int i=0; i<n; i++){
+            if(frequency[s.charAt(i) - 'a'] == 1){
+                return i;
             }
         }
+        return -1;
+    }
+    public static void main(String[] args) {
+        // String s = "leetcode";
+        // String s = "loveleetcode";
+        String s = "aabb";
+        System.out.println(firstUniqChar(s));
     }
 }
