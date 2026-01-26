2e653de561507befaaae7ed21ffb32ff847bf83a
ydv-prince <psychoaimerhere@gmail.com>
2026-01-26 15:52:28 +0530

    53. Maximum Subarray

--- Solution53.java
+++ Solution53.java
@@ -1,19 +1,20 @@
 //53. Maximum Subarray
 public class Solution53 {
     public static int maxSubArray(int[] nums) {
-        int maxSum = nums[0];
         int currSum = nums[0];
+        int maxSum = nums[0];
 
         for(int i=1; i<nums.length; i++){
-            currSum = Math.max(currSum, 0) + nums[i];
-            maxSum = Math.max(maxSum, currSum);
+            currSum = Math.max(currSum, 0)+ nums[i];
+            maxSum = Math.max(currSum, maxSum);
         }
         return maxSum;
     }
     public static void main(String[] args) {
-        int nums[] = {-2,1,-3,4,-1,2,1,-5,4};
+        // int nums[] = {-2,1,-3,4,-1,2,1,-5,4};
         // int nums[] = {5,4,-1,7,8};
-        // int nums[] = {1};
+        int nums[] = {1};
+
 
         System.out.println(maxSubArray(nums));
     }
