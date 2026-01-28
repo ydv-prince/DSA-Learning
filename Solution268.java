e4610bd0254c6675bb96ec48a65e04435f216fbf
ydv-prince <psychoaimerhere@gmail.com>
2026-01-28 23:42:09 +0530

    268. Missing Number

--- Solution268.java
+++ Solution268.java
@@ -1,19 +1,30 @@
 //268. Missing Number
 public class Solution268 {
     public static int missingNumber(int[] nums) {
-       int n = nums.length;
-       int result = n;
+        int n = nums.length;
+    //    int result = n;
 
-        for(int i=0; i<n; i++){
-            result ^= (i ^ nums[i]);
+    //     for(int i=0; i<n; i++){
+    //         result ^= (i ^ nums[i]);
+    //     }
+
+    //     return result;
+    // }
+
+//================================Method: 2========================
+
+        int expectedSum = n*(n+1)/2;
+        int sum=0;
+        for(int num : nums){
+            sum+=num;
         }
-
-        return result;
+        return expectedSum-sum;
     }
+
     public static void main(String[] args) {
         // int[] nums = {3,0,1};
         // int[] nums = {0,1};
-        // int[] nums = {9,6,4,2,3,5,7,0,1};
+        int[] nums = {9,6,4,2,3,5,7,0,1};
 
         System.out.println(missingNumber(nums));
     }
