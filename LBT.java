b97cff4308f4769d998f768636a74024746e2578
ydv-prince <psychoaimerhere@gmail.com>
2026-01-31 23:46:12 +0530

    Basic logics of programming

---
+++ LBT.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class LBT {
+    public static String passOrFail(int mark){
+        if(mark >= 30){
+            return "Pass";
+        }
+        return "Fail";
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your marks: ");
+        int mark = sc.nextInt();
+        System.out.println(passOrFail(mark));
+    }
+}
