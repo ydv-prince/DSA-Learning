3e66bee164ddf1002933d16982e1e981f3e8baa0
ydv-prince <psychoaimerhere@gmail.com>
2026-02-01 23:04:08 +0530

    88. Merge Sorted Array

--- Solution88.java
+++ Solution88.java
@@ -4,30 +4,29 @@ public class Solution88 {
         int last = m+n-1;
 
         while(m>0 && n>0){
-            if(nums1[m-1] > nums2[n-1]){
-                nums1[last] = nums1[m-1];
-                m--;
-            }
-            else{
+            if(nums2[n-1] > nums1[m-1]){
                 nums1[last] = nums2[n-1];
                 n--;
+            }else{
+                nums1[last] = nums1[m-1];
+                m--;
             }
             last--;
         }
 
-        while(n>0){
+        while(n > 0){
             nums1[last] = nums2[n-1];
-            last--;
             n--;
+            last--;
         }
     }
     public static void main(String[] args) {
-        // int[] nums1 = {1,2,3,0,0,0};
-        // int[] nums2 = {2,5,6};
-        // int m = 3;
-        // int n = 3;
-        // merge(nums1, m, nums2, n);
-        // Solution350.printArr(nums1);
+        int[] nums1 = {1,2,3,0,0,0};
+        int[] nums2 = {2,5,6};
+        int m = 3;
+        int n = 3;
+        merge(nums1, m, nums2, n);
+        Solution350.printArr(nums1);
 
         // int[] nums1 = {1};
         // int[] nums2 = {};
@@ -36,11 +35,11 @@ public class Solution88 {
         // merge(nums1, m, nums2, n);
         // Solution350.printArr(nums1);
 
-        int[] nums1 = {0};
-        int[] nums2 = {1};
-        int m = 0;
-        int n = 1;
-        merge(nums1, m, nums2, n);
-        Solution350.printArr(nums1);
+        // int[] nums1 = {0};
+        // int[] nums2 = {1};
+        // int m = 0;
+        // int n = 1;
+        // merge(nums1, m, nums2, n);
+        // Solution350.printArr(nums1);
     }
 }
