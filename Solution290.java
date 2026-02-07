57df97c7a6e10a9c3d77ce7afe8ea8b5e9556526
ydv-prince <psychoaimerhere@gmail.com>
2026-02-07 23:58:00 +0530

    290. Word Pattern

--- Solution290.java
+++ Solution290.java
@@ -5,26 +5,26 @@ public class Solution290 {
         String[] words = s.split(" ");
         if(words.length != pattern.length()) return false;
 
-        HashMap<Character, String> charToWord = new HashMap<>();
-        HashMap<String, Character> wordToChar = new HashMap<>();
+        HashMap<Character, String> charToString = new HashMap<>();
+        HashMap<String, Character> stringToChar = new HashMap<>();
 
         for(int i=0; i<words.length; i++){
-            String w = words[i];
+            String word = words[i];
             char c = pattern.charAt(i);
 
-            if(charToWord.containsKey(c) && !charToWord.get(c).equals(w)) return false;
-            if(wordToChar.containsKey(w) && !wordToChar.get(w).equals(c)) return false;
+            if(charToString.containsKey(c) && !charToString.get(c).equals(word)) return false;
+            if(stringToChar.containsKey(word) && !stringToChar.get(word).equals(c)) return false;
 
-            wordToChar.put(w,c);
-            charToWord.put(c,w);
+            charToString.put(c, word);
+            stringToChar.put(word, c);
         }
         return true;
     }
 
     public static void main(String[] args) {
-        String pattern = "abba", s = "dog cat cat dog";
+        // String pattern = "abba", s = "dog cat cat dog";
         // String pattern = "abba", s = "dog cat cat fish";
-        // String pattern = "aaaa", s = "dog cat cat dog";
+        String pattern = "aaaa", s = "dog cat cat dog";
 
         System.out.println(wordPattern(pattern, s));
     }
