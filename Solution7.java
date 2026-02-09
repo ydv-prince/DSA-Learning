a7ed3529f2ea77e8153d4eab96850ecbd0891177
ydv-prince <psychoaimerhere@gmail.com>
2026-02-09 22:21:41 +0530

    7. Reverse Integer

--- Solution7.java
+++ Solution7.java
@@ -1,9 +1,10 @@
+//7. Reverse Integer
 public class Solution7 {
     public static int reverse(int x) {
         int revresedNum = 0;
 
         while(x != 0){
-            if(revresedNum > Integer.MAX_VALUE || revresedNum < Integer.MIN_VALUE){
+            if(revresedNum > Integer.MAX_VALUE / 10 || revresedNum < Integer.MIN_VALUE / 10){
                 return 0;
             }
 
@@ -13,5 +14,14 @@ public class Solution7 {
         }
 
         return revresedNum;
+    }
+
+    public static void main(String[] args) {
+        // int x = 123;
+        // int x = -123;
+        // int x = 120;
+        int x = 2014748364;
+
+        System.out.println(reverse(x));
     }
 }
