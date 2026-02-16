e64074a43f14af6d2ca1254d6a7d3e68d5cd2c07
ydv-prince <psychoaimerhere@gmail.com>
2026-02-16 22:13:18 +0530

    14. Longest Common Prefix

--- Solution14.java
+++ Solution14.java
@@ -1,22 +1,21 @@
 //14. Longest Common Prefix
+import java.util.Arrays;
 public class Solution14 {
     public static String longestCommonPrefix(String[] strs) {
-        if(strs == null || strs.length == 0) return "";
+        Arrays.sort(strs);
+        String first = strs[0];
+        String last = strs[strs.length-1];
 
-        int numberOfString = strs.length;
-        for(int charIndex=0; charIndex < strs[0].length(); charIndex++){
-            for(int stringIndex = 1; stringIndex < numberOfString; stringIndex++){
-                if(strs[stringIndex].length() <= charIndex || strs[stringIndex].charAt(charIndex) != strs[0].charAt(charIndex)){
-                    return strs[0].substring(0, charIndex);
-                }
-            }
+        int i=0;
+        while(i<first.length() && i<last.length() && first.charAt(i) == last.charAt(i)){
+            i++;
         }
-        return strs[0];
+        return first.substring(0,i);
     }
 
     public static void main(String[] args) {
-        // String strs[] = {"flower","flow","flight"};
-        String strs[] = {"dog","racecar","car"};
+        String strs[] = {"flower","flow","flight"};
+        // String strs[] = {"dog","racecar","car"};
         System.out.println(longestCommonPrefix(strs));
     }
 }
