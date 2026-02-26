b8aec963083ca44269722593e20622be225fd2b6
Prince Kumar <psychoaimerhere@gmail.com>
2026-02-26 23:45:31 +0530

    Helper functions for linked list

---
+++ LInkedLIstHelper.java
@@ -0,0 +1,43 @@
+class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {}
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+public class LInkedLIstHelper {
+    // Build normal linked list from array
+    public static ListNode buildList(int[] values) {
+        if (values.length == 0) return null;
+
+        ListNode head = new ListNode(values[0]);
+        ListNode current = head;
+
+        for (int i = 1; i < values.length; i++) {
+            current.next = new ListNode(values[i]);
+            current = current.next;
+        }
+
+        return head;
+    }
+
+    // Print normal list
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println("null");
+    }
+
+
+}
