9adbe02d142f121d281b14d94328c45061ccdb65
Prince Kumar <psychoaimerhere@gmail.com>
2026-03-03 11:26:09 +0530

    Added main method

--- Solution160.java
+++ Solution160.java
@@ -1,14 +1,4 @@
 //160. Intersection of Two Linked Lists
-
-class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode(int val) {
-            this.val = val;
-        }
-    }
-
 public class Solution160 {
     public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         if(headA == null || headB == null) return null;
@@ -21,5 +11,30 @@ public class Solution160 {
             pointerB = (pointerB == null) ? headA : pointerB.next;
         }
         return pointerA;
+    }
+
+    //Main method
+    public static void main(String[] args) {
+        // Common part
+        ListNode common = new ListNode(8);
+        common.next = new ListNode(4);
+        common.next.next = new ListNode(5);
+
+        // List A
+        ListNode headA = new ListNode(4);
+        headA.next = new ListNode(1);
+        headA.next.next = common;
+
+        // List B
+        ListNode headB = new ListNode(5);
+        headB.next = new ListNode(6);
+        headB.next.next = new ListNode(1);
+        headB.next.next.next = common;
+
+        LInkedLIstHelper.printList(headA);
+        LInkedLIstHelper.printList(headB);
+
+        ListNode result = getIntersectionNode(headA, headB);
+        LInkedLIstHelper.printList(result);
     }
 }
