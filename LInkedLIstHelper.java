b5999196ab037867442e73e5f96b8a5136ed0abf
Prince Kumar <psychoaimerhere@gmail.com>
2026-03-03 11:26:42 +0530

    Updated Helper for cyclic nodes

--- LInkedLIstHelper.java
+++ LInkedLIstHelper.java
@@ -1,23 +1,9 @@
-class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode() {}
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
-    }
+// LinkedListHelper.java
 
 public class LInkedLIstHelper {
-    // Build normal linked list from array
+    // Build list from array
     public static ListNode buildList(int[] values) {
-        if (values.length == 0) return null;
+        if (values == null || values.length == 0) return null;
 
         ListNode head = new ListNode(values[0]);
         ListNode current = head;
@@ -30,7 +16,7 @@ public class LInkedLIstHelper {
         return head;
     }
 
-    // Print normal list
+    // Print list normally
     public static void printList(ListNode head) {
         while (head != null) {
             System.out.print(head.val + " ");
@@ -39,5 +25,40 @@ public class LInkedLIstHelper {
         System.out.println("null");
     }
 
+    // Print limited (for cycle problems)
+    public static void printListLimited(ListNode head, int limit) {
+        int count = 0;
+        while (head != null && count < limit) {
+            System.out.print(head.val + " ");
+            head = head.next;
+            count++;
+        }
+        System.out.println("...");
+    }
 
+    // Build list with cycle (pos = index where tail connects)
+    public static ListNode buildListWithCycle(int[] values, int pos) {
+        if (values == null || values.length == 0) return null;
+
+        ListNode head = new ListNode(values[0]);
+        ListNode current = head;
+        ListNode cycleNode = null;
+
+        if (pos == 0) cycleNode = head;
+
+        for (int i = 1; i < values.length; i++) {
+            current.next = new ListNode(values[i]);
+            current = current.next;
+
+            if (i == pos) {
+                cycleNode = current;
+            }
+        }
+
+        if (pos != -1) {
+            current.next = cycleNode;
+        }
+
+        return head;
+    }
 }
