72e6f604b26ae0cbf8aefe575f32582c00a17078
Prince Kumar <psychoaimerhere@gmail.com>
2026-03-05 13:44:36 +0530

    876. Middle of the Linked List

---
+++ Solution876.java
@@ -0,0 +1,23 @@
+public class Solution876 {
+    public static ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    //Main method
+    public static void main(String[] args) {
+        // int[] values = {1,2,3,4,5};
+        int[] values = {1,2,3,4,5,6};
+
+        ListNode head = LInkedLIstHelper.buildList(values);
+        LInkedLIstHelper.printList(head);
+        ListNode result = middleNode(head);
+        LInkedLIstHelper.printList(result);
+    }
+}
