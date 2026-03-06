49feb080f9f7d92c7e7e63d9fa36e5fe6dd8d525
Prince Kumar <psychoaimerhere@gmail.com>
2026-03-06 21:46:59 +0530

    142. Linked List Cycle II

--- Solution142.java
+++ Solution142.java
@@ -1,14 +1,5 @@
 //142. Linked List Cycle II
 
-class ListNode {
-      int val;
-      ListNode next;
-      ListNode(int x) {
-            val = x;
-            next = null;
-        }
-}
-
 class Solution142 {
     static ListNode detectCycle(ListNode head) {
         if (head == null) return null;
