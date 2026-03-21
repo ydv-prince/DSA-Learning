599f1d90ab3460cbd51f1edb64d24369403c00fc
Prince Kumar <psychoaimerhere@gmail.com>
2026-03-21 23:38:19 +0530

    844. Backspace String Compare

--- Solution844.java
+++ Solution844.java
@@ -1,48 +1,31 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 //844. Backspace String Compare
 public class Solution844 {
     public static boolean backspaceCompare(String s, String t) {
-        int i = s.length()-1 , j = t.length()-1;
+        return build(s).equals(build(t));
+    }
 
-        int skipS = 0, skipT = 0;
-
-        while(i >= 0 || j >=0){
-            while(i >= 0){
-                if(s.charAt(i) == '#'){
-                    skipS++;
-                    i--;
-                }
-                else if(skipS > 0){
-                    skipS--;
-                    i--;
-                }
-                else{
-                    break;
+    private static String build(String s){
+        Deque<Character> stack = new ArrayDeque<>();
+        for(char c : s.toCharArray()){
+            if(c == '#'){
+                if(!stack.isEmpty()){
+                    stack.pop();
                 }
             }
-
-                while(j >= 0){
-                    if(t.charAt(j) == '#'){
-                        skipT++;
-                        j--;
-                    }
-                    else if(skipT > 0){
-                        skipT--;
-                        j--;
-                    }
-                    else{
-                        break;
-                    }
-                }
-                if(i >= 0 && j >= 0 && s.charAt(i) != t.charAt(j)){
-                    return false;
-                }
-                if((i >= 0) != (j >= 0)){
-                    return false;
-                }
-                i--;
-                j--;
+            else{
+                stack.push(c);
+            }
         }
-        return true;
+
+        StringBuilder result = new StringBuilder();
+        while(!stack.isEmpty()){
+            result.append(stack.pop());
+        }
+
+        return result.toString();
     }
     //Main method
     public static void main(String[] args) {
