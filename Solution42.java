2f65ae39fdfba8362b09a17ae3d83bd56e8f42e3
Prince Kumar <psychoaimerhere@gmail.com>
2026-03-23 11:04:35 +0530

    42. Trapping Rain Water

---
+++ Solution42.java
@@ -0,0 +1,30 @@
+public class Solution42 { 
+    public static int trap(int[] height) {
+        int n = height.length;
+        int[] leftMax = new int[n];
+        int[] rightMax = new int[n];
+
+        leftMax[0] = height[0];
+        rightMax[n-1] = height[n-1];
+
+        for(int i=1; i<n; i++){
+            leftMax[i] = Math.max(leftMax[i-1], height[i]);
+            rightMax[n-i-1] = Math.max(rightMax[n-i], height[n-i-1]);
+        }
+
+        int totalWater = 0;
+        for(int i=0; i<n; i++){
+            totalWater += Math.min(leftMax[i], rightMax[i]) - height[i];
+        }
+
+        return totalWater;
+    }
+
+    //Main method
+    public static void main(String[] args) {
+        // int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
+        int[] height = {4,2,0,3,2,5};
+
+        System.out.println(trap(height));
+    }
+}
