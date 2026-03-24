ba13ddfe57f96822b2c610fcb7cade1fe824fd7a
Prince Kumar <psychoaimerhere@gmail.com>
2026-03-25 00:47:48 +0530

    76. Minimum Window Substring

---
+++ Solution76.java
@@ -0,0 +1,15 @@
+//76. Minimum Window Substring
+public class Solution76 {
+    public static String minWindow(String s, String t) {
+        
+    }
+
+    //Main method
+    public static void main(String[] args) {
+        String s = "ADOBECODEBANC", t = "ABC";
+        // String s = "a", t = "a";
+        // String s = "a", t = "aa";
+
+        System.out.println(minWindow(s, t));
+    }
+}
