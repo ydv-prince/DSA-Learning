6d9dd3b88e2c1e83242d370da1beeb94c355dea7
Prince Kumar <psychoaimerhere@gmail.com>
2026-03-25 00:43:24 +0530

    19. Remove Nth Node From End of List

--- Solution19.java
+++ Solution19.java
@@ -1,37 +1,35 @@
 //19. Remove Nth Node From End of List
 class Solution19 {
     public static ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode dummy = new ListNode(0);
+        ListNode dummy = new ListNode();
         dummy.next = head;
 
-        ListNode fastPointer = dummy;
-        ListNode slowPointer = dummy;
-
+        ListNode fast = dummy;
+        ListNode slow = dummy;
         while(n>0){
-            fastPointer = fastPointer.next;
+            fast = fast.next;
             n--;
         }
 
-        while(fastPointer.next != null){
-            fastPointer = fastPointer.next;
-            slowPointer = slowPointer.next;
+        while(fast.next != null){
+            fast = fast.next;
+            slow = slow.next;
         }
 
-        slowPointer.next = slowPointer.next.next;
-
+        slow.next = slow.next.next;
         return dummy.next;
     }
 
     //Main method
     public static void main(String[] args) {
-        // int[] values = {1,2,3,4,5};
-        // int n = 2;
+        int[] values = {1,2,3,4,5};
+        int n = 2;
 
         // int[] values = {1};
         // int n = 1;
 
-        int[] values = {1,2};
-        int n = 1;
+        // int[] values = {1,2};
+        // int n = 1;
 
         ListNode head = LInkedLIstHelper.buildList(values);
         LInkedLIstHelper.printList(head);
