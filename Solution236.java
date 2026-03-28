8fe1f4beefaf6cc3355d8ee89925e2710243b83b
Prince Kumar <psychoaimerhere@gmail.com>
2026-03-28 23:31:46 +0530

    236. Lowest Common Ancestor of a Binary Tree

---
+++ Solution236.java
@@ -0,0 +1,17 @@
+//236. Lowest Common Ancestor of a Binary Tree
+public class Solution236 {
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q){
+        if(root == p || root == q){
+            return root;
+        }
+
+        TreeNode leftLCA = lowestCommonAncestor(root.left, p, q);
+        TreeNode rightLCA = lowestCommonAncestor(root.right, p, q);
+
+        if(leftLCA != null && rightLCA != null){
+            return root;
+        }
+
+        return leftLCA == null ? rightLCA : leftLCA;
+    }
+}
