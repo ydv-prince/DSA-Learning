66c3b3407ccad414602803167f61697b52133d06
Prince Kumar <psychoaimerhere@gmail.com>
2026-04-03 23:56:04 +0530

    76. Minimum Window Substring

--- Solution76.java
+++ Solution76.java
@@ -1,7 +1,45 @@
 //76. Minimum Window Substring
 public class Solution76 {
     public static String minWindow(String s, String t) {
-        
+        int[] targetFreq = new int[128];
+        int[] windowFreq = new int[128];
+      
+        for (char ch : t.toCharArray()) {
+            targetFreq[ch]++;
+        }
+      
+        int sourceLen = s.length();
+        int targetLen = t.length();
+      
+        int minWindowStart = -1;
+        int minWindowLen = sourceLen + 1;
+        int validCharCount = 0;
+      
+        int left = 0;
+        for (int right = 0; right < sourceLen; right++) {
+            char rightChar = s.charAt(right);
+            windowFreq[rightChar]++;
+          
+            if (windowFreq[rightChar] <= targetFreq[rightChar]) {
+                validCharCount++;
+            }
+          
+            while (validCharCount == targetLen) {
+                if (right - left + 1 < minWindowLen) {
+                    minWindowLen = right - left + 1;
+                    minWindowStart = left;
+                }
+
+                char leftChar = s.charAt(left);
+                if (windowFreq[leftChar] <= targetFreq[leftChar]) {
+                    validCharCount--;
+                }
+              
+                windowFreq[leftChar]--;
+                left++;
+            }
+        }
+        return minWindowStart < 0 ? "" : s.substring(minWindowStart, minWindowStart + minWindowLen);
     }
 
     //Main method
