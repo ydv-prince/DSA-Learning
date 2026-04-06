45f632cf2194b9067b35e83f517eb59369575e95
Prince Kumar <psychoaimerhere@gmail.com>
2026-04-06 22:43:29 +0530

    100. Same Tree

---
+++ Solution100.java
@@ -0,0 +1,12 @@
+//100. Same Tree
+public class Solution100 {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == q){ return true; }
+
+        if(p == null || q == null || p.val != q.val){
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
