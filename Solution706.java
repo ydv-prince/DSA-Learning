618c0090cccf9eb16797decf23551030ae12710c
Prince Kumar <psychoaimerhere@gmail.com>
2026-04-07 22:27:30 +0530

    706. Design HashMap

---
+++ Solution706.java
@@ -0,0 +1,23 @@
+//706. Design HashMap
+import java.util.Arrays;
+public class Solution706{
+    class MyHashMap {
+        private int[] data;
+        public MyHashMap() {
+            data = new int[1000001];
+            Arrays.fill(data, -1);
+    }
+        
+        public void put(int key, int value) {
+            data[key] = value;
+        }
+        
+        public int get(int key) {
+            return data[key];
+        }
+        
+        public void remove(int key) {
+            data[key] = -1;
+        }
+    }
+}
