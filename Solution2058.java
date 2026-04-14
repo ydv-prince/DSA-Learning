fa1e1546b0f567876f843869601796fe25837022
Prince Kumar <psychoaimerhere@gmail.com>
2026-04-15 00:14:29 +0530

    2058. Find the Minimum and Maximum Number of Nodes Between Critical Points

--- Solution2058.java
+++ Solution2058.java
@@ -1,7 +1,7 @@
 //2058. Find the Minimum and Maximum Number of Nodes Between Critical Points
 public class Solution2058{
     public static int[] nodesBetweenCriticalPoints(ListNode head) {
-        int[] result = {Integer.MAX_VALUE, 0git add };
+        int[] result = {Integer.MAX_VALUE, 0 };
 
         int firstCriticalIndex = -1;
         int lastCriticalIndex = -1;
