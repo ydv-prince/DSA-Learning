83355aa4831e17e3276c2b5311ac526cc4390221
ydv-prince <psychoaimerhere@gmail.com>
2026-04-29 23:41:04 +0530

    875. Koko Eating Bananas

---
+++ Solution875.java
@@ -0,0 +1,39 @@
+//875. Koko Eating Bananas
+public class Solution875 {
+    public static int minEatingSpeed(int[] piles, int h) {
+        int max = 0;
+        for(int pile : piles){
+            max = Math.max(max, pile);
+        }
+
+        int left = 1;
+        int right = max;
+        int firstIdx = -1;
+
+        while(left <= right){
+            int mid = left + (right-left) / 2;
+
+            int totalHours = 0;
+            for(int pile : piles){
+                totalHours += (pile + mid -1) / mid;
+            }
+
+            if(totalHours <= h){
+                firstIdx = mid;
+                right = mid-1;
+            }
+            else{
+                left = mid + 1;
+            }
+        }
+        return firstIdx;
+    }
+
+    public static void main(String[] args) {
+        // int[] piles = {3,6,7,11}; int h = 8;
+        // int[] piles = {30,11,23,4,20}; int h = 5;
+        int[] piles = {30,11,23,4,20}; int h = 6;
+
+        System.out.println(minEatingSpeed(piles, h));
+    }
+}
