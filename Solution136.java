3458ec2c1d98b7f9a47ccf92e0a61d3c06bda099
Prince Kumar <pkumar052@rku.ac.in>
2026-05-05 23:07:43 +0530

    136. Single Number

--- Solution136.java
+++ Solution136.java
@@ -1,26 +1,19 @@
 //136. Single Number
-import java.util.HashSet;
-import java.util.Set;
 public class Solution136{
     public static int singleNumber(int[] nums) {
-        Set<Integer> numSet = new HashSet<>();
+        int result = 0;
 
-        for(int n : nums){
-            if(numSet.contains(n)){
-                numSet.remove(n);
-            }
-            else{
-                numSet.add(n);
-            }
+        for(int num : nums){
+            result ^= num;
         }
         
-        return numSet.iterator().next();
+        return result;
     }
 
     public static void main(String[] args) {
-        // int[] nums = {2,2,1};
+        int[] nums = {2,2,1};
         // int[] nums = {4,1,2,1,2};
-        int[] nums = {1};
+        // int[] nums = {1};
 
         System.out.println(singleNumber(nums));
     }
