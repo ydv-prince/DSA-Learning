c1ce418a6d419da577f3fdf0641ad67086f08015
Prince Kumar <psychoaimerhere@gmail.com>
2026-08-19 21:55:02 +0530

    Time: 0 ms (100%), Space: 43.8 MB (47.8%)

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int last = m+n-1;
+
+        while(m>0 && n>0){
+            if(nums1[m-1] >= nums2[n-1]){
+                nums1[last] = nums1[m-1];
+                m--;
+            }
+            else{
+                nums1[last] = nums2[n-1];
+                n--;
+            }
+            last--;
+        }
+
+        while(n > 0){
+            nums1[last] = nums2[n-1];
+            n--;
+            last--;
+        }
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
