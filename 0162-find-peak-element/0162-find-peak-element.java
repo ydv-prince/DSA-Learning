3d9016c1d4c151d9913a6bc0eeed6913d19b5ac9
Prince Kumar <psychoaimerhere@gmail.com>
2026-09-04 21:55:12 +0530

    Time: 0 ms (100%), Space: 44.2 MB (38.29%)

---
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int n = nums.length;
+
+        if(n == 1){ return 0; }
+
+        if(nums[0] > nums[1]){ return 0; }
+
+        if(nums[n-1] > nums[n-2]){ return n-1; }
+
+        int left = 1, right = n-2;
+
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+
+            if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]){
+                return mid;
+            }
+
+            // Peak >> Right Half
+            else if(nums[mid] > nums[mid-1]){
+                left = mid+1;
+            }
+
+            //Peak >> Left Half
+            else{
+                right = mid-1;
+            }
+        }
+        return -1;
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
