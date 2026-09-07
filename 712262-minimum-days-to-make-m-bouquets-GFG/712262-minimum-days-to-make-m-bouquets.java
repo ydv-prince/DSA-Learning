9bb6153a86b9ec68e90f5357c3d0f60063493077
Prince Kumar <psychoaimerhere@gmail.com>
2026-09-07 09:46:19 +0530

    Minimum Days to Make m Bouquets

---
+++ 712262-minimum-days-to-make-m-bouquets-GFG/712262-minimum-days-to-make-m-bouquets.java
@@ -0,0 +1,57 @@
+class Solution {
+    public int minDaysBloom(int[] arr, int k, int m) {
+        int n = arr.length;
+        
+        if(n < (m*k)){
+            return -1;
+        }
+        
+        int mini = Integer.MAX_VALUE;
+        int maxi = Integer.MIN_VALUE;
+        
+        for(int num : arr){
+            maxi = Math.max(maxi, num);
+            mini = Math.min(mini, num);
+        }
+        
+        int low = mini, high = maxi;
+        
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            
+            if(possible(arr, mid, k, m)){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        
+        return low;
+    }
+    
+    private boolean possible(int[] arr, int day, int k, int m){
+        
+        int count = 0;
+        int bouquets = 0;
+        
+        for(int num : arr){
+            if(num <= day){
+                count++;
+            }
+            else{
+                bouquets += count / k;
+                count=0;
+            }
+        }
+        
+        bouquets += count / k;
+        count=0;
+        
+        return bouquets >= m;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
