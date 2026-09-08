4be27c3a0d53aa874a25b5c103f09d8d8bb1e331
Prince Kumar <psychoaimerhere@gmail.com>
2026-09-08 21:59:38 +0530

    Time: 1 ms (83.65%), Space: 42.6 MB (39.85%)

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int countCommas(int n) {
+        return Math.max(n - 999, 0);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
