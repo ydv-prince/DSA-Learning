baee678c4c2d743c1a2ca4b99bc5db29a8642174
Prince Kumar <psychoaimerhere@gmail.com>
2026-09-17 14:00:47 +0530

    Time: 0 ms (100%), Space: 43.4 MB (14.65%)

---
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private List<Integer> result = new ArrayList<>();
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        performPreOrderDFS(root);
+        return result;
+    }
+
+    private void performPreOrderDFS(TreeNode node){
+        if(node == null){ return; }
+
+        result.add(node.val);
+        performPreOrderDFS(node.left);
+        performPreOrderDFS(node.right);
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
