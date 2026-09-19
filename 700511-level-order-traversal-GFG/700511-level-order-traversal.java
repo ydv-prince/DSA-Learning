6d6f22f3f8c772b08947ba9cda3c8074a151f2bc
Prince Kumar <psychoaimerhere@gmail.com>
2026-09-19 17:10:49 +0530

    Level Order Traversal(BFS)

---
+++ 700511-level-order-traversal-GFG/700511-level-order-traversal.java
@@ -0,0 +1,40 @@
+/* Structure of Binary Tree Node
+class Node {
+    public int data;
+    public Node left;
+    public Node right;
+
+    // Constructor
+    public Node(int val) {
+        data = val;
+        left = right = null;
+    }
+};*/
+
+class Solution {
+    public ArrayList<Integer> levelOrder(Node root) {
+        ArrayList<Integer> res = new ArrayList<>();
+        
+        if(root == null){ return res; }
+        
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        
+        while(!queue.isEmpty()){
+            Node node = queue.poll();
+            res.add(node.data);
+            
+            if(node.left != null){
+                queue.add(node.left);
+            }
+            if(node.right != null){
+                queue.add(node.right);
+            }
+        }
+        return res;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
