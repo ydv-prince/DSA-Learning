8b8b4971d96a0f6d740d05878c37645562f133ab
Prince Kumar <psychoaimerhere@gmail.com>
2026-09-20 09:45:49 +0530

    PreOrder Traversal - Iterative

---
+++ 700319-preorder-traversal-GFG/700319-preorder-traversal.java
@@ -0,0 +1,38 @@
+/* Structure of Tree Node
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}*/
+
+class Solution {
+    public ArrayList<Integer> preOrder(Node root) {
+        ArrayList<Integer> res = new ArrayList<>();
+        if(root == null){ return res; }
+        
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        
+        while(!stack.isEmpty()){
+            root = stack.pop();
+            res.add(root.data);
+            
+            if(root.right != null){
+                stack.push(root.right);
+            }
+            if(root.left != null){
+                stack.push(root.left);
+            }
+        }
+        return res;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
