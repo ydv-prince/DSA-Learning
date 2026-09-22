0280eff11b074f7418c329fcfba5386d04cafd0e
Prince Kumar <psychoaimerhere@gmail.com>
2026-09-22 09:05:04 +0530

    Time: 0 ms (100%), Space: 43 MB (71.36%) - 1 Stack(Iterative)

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -14,33 +14,20 @@
  * }
  */
 class Solution {
+    private List<Integer> result = new ArrayList<>();
     public List<Integer> postorderTraversal(TreeNode root) {
-        ArrayList<Integer> res = new ArrayList<>();
-        if(root == null){ return res; }
-
-        Stack<TreeNode> st1 = new Stack<>();
-        Stack<TreeNode> st2 = new Stack<>();
-        
-        st1.push(root);
-        while(!st1.isEmpty()){
-            root = st1.pop();
-            st2.add(root);
-            
-            if(root.left != null){
-                st1.push(root.left);
-            }
-
-            if(root.right != null){
-                st1.push(root.right);
-            }
-        }
-        
-        while(!st2.isEmpty()){
-            res.add(st2.pop().val);
-        }
-        
-        return res;
+        performPostOrderDFS(root);
+        return result;
     }
+
+    private void performPostOrderDFS(TreeNode node){
+        if(node == null){ return; }
+
+        performPostOrderDFS(node.left);
+        performPostOrderDFS(node.right);
+        result.add(node.val);
+    }
+
 }
 
 // Synced seamlessly with LeetHub Pro
