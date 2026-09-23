306e892e0b18891b225362d8f92666e226265312
Prince Kumar <psychoaimerhere@gmail.com>
2026-09-23 09:02:34 +0530

    Height of Binary Tree

---
+++ 700164-height-of-binary-tree-GFG/700164-height-of-binary-tree.java
@@ -0,0 +1,28 @@
+/* Structure of Binary Tree Node
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int val) {
+        data = val;
+        left = right = null;
+    }
+}*/
+
+class Solution {
+    public int height(Node root) {
+        if(root == null){
+            return -1;
+        }
+        
+        int lh = height(root.left);
+        int rh = height(root.right);
+
+        return 1 + Math.max(lh, rh);
+        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
